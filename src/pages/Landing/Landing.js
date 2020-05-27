c90cd67bed56aa33926e5371d08722425fda6c2a
Agoi Abel <agoiabeladeyemi@gmail.com>
2020-05-27 22:25:52 +0100

    working on the landing page

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,45 +1,58 @@
 import React from 'react';
-import arrow from '../../images/arrow.svg';
-import AuthBg1 from '../../images/auth1.svg';
-import AuthBg2 from '../../images/auth2.svg';
 import Nav from '../../components/Navbar';
 import styles from './Landing.module.scss';
+import arrow from '../../images/arrow.svg';
+import AuthBg1 from '../../images/auth1.svg';
+import Button from '../../components/Button';
+import AuthBg2 from '../../images/auth2.svg';
 import landingImg1 from '../../images/tk1.png';
 import landingImg2 from '../../images/tk2.png';
-import Button from '../../components/Button';
 import logo from '../../images/urban24-logo.png';
 import LayoutDefault from '../../components/LayoutDefault';
 
 const Landing = () => {
-    const theme = "white"
     return (
-        <LayoutDefault title={'Landing Page'}>
-            <div className={styles.landing}>
-                <img className={styles.landing_bg1} src={AuthBg1} alt="Urban24 sign up" />
-                <img className={styles.landing_bg2} src={AuthBg2} alt="Urban24 sign up" />
-                <div className={styles.landing_content}>
-                    <div style={{ padding: '0 3rem'}}>
-                        <Nav logo={logo} light={theme} />
-                    </div>
-                    <div className={styles.landing_content_carousel}>
-                        <div className={styles.hero}>
-                            <img src={landingImg1} alt="Urban24 star thumbnail" />
-                        </div>
-                        <h4>
-                            Star in <br />
-                            <span style={{ paddingLeft: '30rem' }}>Every Light</span>
-                        </h4>
-                        <div className={styles.content_text}>
-                            <p>We often talk about React components as lego building blocks to build our applications, and I think that when people hear this, they somehow think this excludes the state aspect. The "secret" behind my personal solution to the state management problem is to think of how your application's state maps to the application's tree structure.</p>
-                            <div style={{ padding: '0 2rem'}}>
-                                <Button type={'btn__transparent__light'} size={'btn__large'}>Enter Contest</Button>
-                            </div>
-                        </div>
-                        <div className={styles.arrow}>
-                            <img src={arrow} alt="Urban24 carousel icon" />
-                        </div>
+        <LayoutDefault>
+
+            <div className={styles.container}>
+                <div className={styles.wrapper}>
+                    <div className={styles.logo}>
+                        <img src={require('../../images/urban24-logo.png')} alt="Urban24 logo" />
                     </div>
                 </div>
+
+                <div className={styles.banner_image}>
+                    <img src={require('../../images/landing_banner_sm.png')} /> 
+                </div>
+
+                <div className={styles.banner_one_content}>
+                    <div className={styles.wrapper}>
+                        <div className={styles.banner_content_header}>
+                            Star in Every Light
+                        </div>
+                        <p className={styles.banner_content_description}>
+                            Urban 24 is a modeling search by leading smartphone brand OPPO, 
+                            geared to discover the stars of tomorrow and launch the careers
+                            of aspiring models dreaming of becoming one of the glamorous 
+                            few to represent Nigeria to the world.
+                        </p>
+                    </div>
+                </div>
+
+                <div className={styles.banner_two_content}>
+                    <div className={styles.wrapper}>
+                        <div className={styles.banner_content_header}>
+                            Participate to Win
+                        </div>
+                        <p className={styles.banner_content_description}>
+                            Urban 24 is a modeling search by leading smartphone brand OPPO, 
+                            geared to discover the stars of tomorrow and launch the careers
+                            of aspiring models dreaming of becoming one of the glamorous 
+                            few to represent Nigeria to the world.
+                        </p>
+                    </div>
+                </div>
+
             </div>
         </LayoutDefault>
     )
