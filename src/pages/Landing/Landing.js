0313aee79e30526f7a23f80f1ec41a1d382966ae
Agoi Abel <agoiabeladeyemi@gmail.com>
2020-05-28 12:36:46 +0100

    Landing page mobile version

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Nav from '../../components/Navbar';
 import styles from './Landing.module.scss';
 import arrow from '../../images/arrow.svg';
@@ -11,6 +12,11 @@ import logo from '../../images/urban24-logo.png';
 import LayoutDefault from '../../components/LayoutDefault';
 
 const Landing = () => {
+    
+    const popUp = () => {
+        console.dir('popUp');
+    }
+    
     return (
         <LayoutDefault>
 
@@ -40,23 +46,92 @@ const Landing = () => {
                 </div>
 
                 <div className={styles.banner_two_content}>
-                    <div className={styles.wrapper}>
-                        <div className={styles.banner_content_header}>
+                    <div className={styles.banner_two_content_description}>
+                        <div className={styles.banner_two_content_header}>
                             Participate to Win
                         </div>
-                        <p className={styles.banner_content_description}>
-                            Urban 24 is a modeling search by leading smartphone brand OPPO, 
-                            geared to discover the stars of tomorrow and launch the careers
-                            of aspiring models dreaming of becoming one of the glamorous 
-                            few to represent Nigeria to the world.
+                        <p className={styles.banner_two_content_text}>
+                            <div className={styles.banner_two_content_image}>
+                                <img src={require('../../images/reno_mobile.png')} />
+                            </div>
+
+                            <div className={styles.items}>
+                                <div className={styles.item}>
+                                    <div className={styles.item_number}>1</div>
+                                    <div className={styles.item_description}>One Million Naira (N1,000,000)</div>
+                                </div>
+                                <div className={`${styles.item} ${styles.item_two}`}>
+                                    <div className={styles.item_number}>2</div>
+                                    <div className={styles.item_description}>OPPO Reno3 Smartphone</div>
+                                </div>
+                                <div className={`${styles.item} ${styles.item_three}`}>
+                                    <div className={styles.item_number}>3</div>
+                                    <div className={styles.item_description}>An exclusive deal to Star the cover of Genevieve Magazine</div>
+                                </div>
+                            </div>
                         </p>
                     </div>
                 </div>
 
+                <div className={styles.banner_three_content}>
+                    <div className={styles.wrapper}>
+                        <div className={styles.banner_content_header}>
+                            How to Enter
+                        </div>
+                        <div className={styles.banner_three_items}>
+                            <div className={styles.banner_three_item}>
+                                <div className={styles.item}>
+                                    <div className={styles.item_number}>1</div>
+                                    <div className={styles.hr}></div>
+                                </div>
+                                <p className={styles.banner_content_description}>
+                                    Fill the registration form with all required information. 
+                                    Upload your top 5 photos showing you star in different lightings.
+                                </p>
+                            </div>
+
+                            <div className={`${styles.banner_three_item} ${styles.banner_three_item__alternate}`}>
+                                <div className={styles.item}>
+                                    <div className={styles.item_number}>2</div>
+                                    <div className={styles.hr}></div>
+                                </div>
+                                <p className={styles.banner_content_description}>
+                                    Upon successful completion, 
+                                    follow the prompt to share your participation 
+                                    in the Urban24 contest on your social media.
+                                </p>
+                            </div>
+
+                            <div className={styles.banner_three_item}>
+                                <div className={styles.item}>
+                                    <div className={styles.item_number}>3</div>
+                                    <div className={styles.hr}></div>
+                                </div>
+                                <p className={styles.banner_content_description}>
+                                    Fill the registration form with all required information. 
+                                    Upload your top 5 photos showing you star in different lightings.
+                                </p>
+                            </div>
+                        </div>
+
+                        <Button onClick={() => popUp()} type={'btn__primary__outline'} size={'btn__large'}>Enter Contest</Button>
+                    </div>
+                </div>
+
+                <div className={`${styles.footer} ${styles.wrapper}`}>
+                    <div>
+                        <Link to={'/terms'} className={styles.link}>Terms and Conditions</Link>
+                    </div>
+                    <div>
+                        <Link to={'/about'} className={styles.link}>About</Link>
+                    </div>
+                    <div>
+                        <Link to={'/'} className={styles.link}>OPPO.com</Link>
+                    </div>
+                </div>
             </div>
         </LayoutDefault>
     )
 }
 
 export default Landing;
-
