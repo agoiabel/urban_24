63776a8e600391c1a85568a8d0ce1f7b31bb25cd
Agoi Abel <agoiabeladeyemi@gmail.com>
2020-05-31 02:35:58 +0100

    Before installation of framer motion

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -4,11 +4,11 @@ import { useForm } from "react-hook-form";
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import AuthLayout from '../../components/AuthLayout';
+import { CSSTransition } from 'react-transition-group';
 import { useDispatch, useSelector } from "react-redux";
 import { MdEmail, MdLockOutline } from "react-icons/md";
 import { loginCreate } from '../../shared/actions/Login.action';
 import authStyles from '../../components/AuthLayout/AuthLayout.module.scss';
-
 const Login = props => {
 
     const dispatch = useDispatch();
@@ -47,23 +47,28 @@ const Login = props => {
                 <img src={require('../../images/urban24-logo.png')} alt="Urban24 logo" />
             </div>
             <div className={authStyles.auth_form}>
-                <div className={authStyles.auth_form_box}>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <Input type={'email'} name="email" control={control} hasIcon icon={emailIcon} placeholder="Email Address" errors={errors.email} rules={{required: "email is a required",pattern: {
-                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                            message: "invalid email address"
-                        }}} />
 
-                        <Input type={"password"} name="password" control={control} hasIcon icon={passwordIcon} placeholder="Password" errors={errors.password} rules={{required: "password is a required"}} />
+                <CSSTransition in={true} timeout={1000} classNames="auth_form_box">
+                    <div className={authStyles.auth_form_box}>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <Input type={'email'} name="email" control={control} hasIcon icon={emailIcon} placeholder="Email Address" errors={errors.email} rules={{required: "email is a required",pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                message: "invalid email address"
+                            }}} />
 
-                        <Button type={'btn__primary__solid'} size={'btn__large'} isLoading={isLoading} disabled={!formState.isValid}>Login</Button>
-                    </form>
-                    <div className={authStyles.auth_link}>
-                        <Link to="/forgotpassword" as="/forgotpassword" className="link-text">
-                            Forgot password
-                        </Link>
+                            <Input type={"password"} name="password" control={control} hasIcon icon={passwordIcon} placeholder="Password" errors={errors.password} rules={{required: "password is a required"}} />
+
+                            <Button type={'btn__primary__solid'} size={'btn__large'} isLoading={isLoading} disabled={!formState.isValid}>Login</Button>
+                        </form>
+                        <div className={authStyles.auth_link}>
+                            <Link to="/forgotpassword" as="/forgotpassword" className="link-text">
+                                Forgot password
+                            </Link>
+                        </div>
                     </div>
-                </div>
+                </CSSTransition>
+    
+
             </div>
         </AuthLayout>
     )
